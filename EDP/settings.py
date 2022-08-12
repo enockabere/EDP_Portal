c4e81a21d1ae632e8e13b5ec7d9955d7bbcf1c68
enockabere <maebaenock95@gmail.com>
2022-08-12 10:16:00 +0300

    deploy

--- EDP/settings.py
+++ EDP/settings.py
@@ -180,7 +180,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
